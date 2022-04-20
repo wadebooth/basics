c15cc0062e0c332cf0ba1bc3b8aea26c2bc0f3b5
wadebooth <wademgmt@gmail.com>
2022-04-20 11:28:51 -0400

    My initial commit

---
+++ objectarrays.js
@@ -0,0 +1,17 @@
+let instructor = {
+    firstName: 'Todd',
+    lastName: 'Albert',
+    kids: [{
+        firstName: 'Sage',
+        age: 21,
+        gender: 'female',
+    }, {
+        firstName: 'Noah',
+        age: 18,
+        gender: 'male',
+    }],
+    pets: {
+        dogs: ['Ryder'],
+        cats: null,
+    },
+}
